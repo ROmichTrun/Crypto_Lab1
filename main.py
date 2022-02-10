fe088b062ad9cc5b805b8de846326da53cb603a3
ROmichTrun <73660524+ROmichTrun@users.noreply.github.com>
2022-02-10 21:59:36 +0300

    New version

--- main.py
+++ main.py
@@ -18,37 +18,78 @@ while count != 16:
         kardano[7 - i, 7 - j] = 2
         kardano[7 - j, i] = 2
         count += 1
-print(kardano)
-for i in range(0,8):
+#print(kardano)
+for i in range(0, 8):
     for j in range(0, 8):
         if kardano[i, j] == 2:
             kardano[i, j] = 0
 print(kardano)
-#print("Введите сообщение для шифрования(до 64 символов)")
-#message = input()
-#message_x = ''
-#z = len(message)
-#for i in range(0, 64):
-#    if i < len(message):
-#        message_x += message[i]
-#    else:
-#        message_x += '0'
-#print(message_x)
+print("Введите сообщение для шифрования(до 64 символов)")
+message = input()
+message_x = ''
+z = len(message)
+for i in range(0, 64):
+    if i < len(message):
+        message_x += message[i]
+    else:
+        message_x += '0'
+print(message_x)
 
-kardano1 = np.zeros((8,8))
-kardano2 = np.zeros((8,8))
-kardano3 = np.zeros((8,8))
-kardano4 = np.zeros((8,8))
-for i in range(0,8):
+kardano1 = np.zeros((8, 8))
+kardano2 = np.zeros((8, 8))
+kardano3 = np.zeros((8, 8))
+kardano4 = np.zeros((8, 8))
+for i in range(0, 8):
     for j in range(0, 8):
         kardano1[i, j] = kardano[7 - j, i]
         kardano2[i, j] = kardano[7 - i, 7 - j]
         kardano3[i, j] = kardano[j, 7 - i]
-for i in range(0,8):
+for i in range(0, 8):
     for j in range(0, 8):
-        if kardano[i, j] == 1 or kardano1[i, j] == 1 or kardano2[i, j] == 1 or kardano3[i, j] == 1:
-            kardano4[i, j] = 1
+        kardano4[i, j] = kardano[i, j] + kardano1[i, j] + kardano2[i, j] + kardano3[i, j]
 print(kardano4)
 
+encrypted = np.array([['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0', '0', '0']])
+ind = 0
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano[i, j] == 1:
+            encrypted[i, j] = message_x[ind]
+            ind +=1
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano1[i, j] == 1:
+            encrypted[i, j] = message_x[ind]
+            ind +=1
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano2[i, j] == 1:
+            encrypted[i, j] = message_x[ind]
+            ind +=1
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano3[i, j] == 1:
+            encrypted[i, j] = message_x[ind]
+            ind +=1
+print(encrypted)
 
 
+decrypted = ''
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano[i, j] == 1:
+            decrypted += encrypted[i, j]
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano1[i, j] == 1:
+            decrypted += encrypted[i, j]
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano2[i, j] == 1:
+            decrypted += encrypted[i, j]
+for i in range(0, 8):
+    for j in range(0, 8):
+        if kardano3[i, j] == 1:
+            decrypted += encrypted[i, j]
+print(message_x)
+print(decrypted)
